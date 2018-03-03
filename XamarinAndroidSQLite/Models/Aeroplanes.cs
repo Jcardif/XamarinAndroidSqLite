d385cbf9ecab683b701cdc733ecb1c0fc8802a9b
Jcardif <josh.cardif14@outlook.com>
2018-03-03 16:38:27 +0300

    Added the model

---
+++ XamarinAndroidSQLite/Models/Aeroplanes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using XamarinAndroidSQLite.ORM;
+
+namespace XamarinAndroidSQLite.Models
+{
+    [Table("aeroplanes")]
+   public class Aeroplanes
+    {
+        [PrimaryKey, AutoIncrement, Column("id")]
+        public int Id { get; set; }
+        [MaxLength(21), Column("name")]
+        public string Name { get; set; }
+    }
+}
---
+++ XamarinAndroidSQLite/ORM/DatabaseRepository.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using Android.Widget;
+using Mono.Data.Sqlite;
+using XamarinAndroidSQLite.Models;
+
+namespace XamarinAndroidSQLite.ORM
+{
+    public class DatabaseRepository
+    {
+        public string CreateDatabase()
+        {
+            var output = "";
+            output += "Creating Database";
+            string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal), "myPlanes.db3");
+            var db=new SQLiteConnection(dbPath);
+            output += "\n database created successfully";
+            return output;
+        }
+
+        public string CreateTable()
+        {
+            try
+            {
+                var dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal),
+                    "myplanes.db3");
+                var db = new SQLiteConnection(dbPath);
+                db.CreateTable<Aeroplanes>();
+                return "Table created successfully";
+            }
+            catch (Exception ex)
+            {
+                return ex.Message;
+            }
+        }
+
+        public string InserRecord(string name)
+        {
+            try
+            {
+                string dbPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Personal),
+                    "myplanes.db3");
+                var db=new SQLiteConnection(dbPath);
+                Aeroplanes aero=new Aeroplanes();
+                aero.Name = name;
+                db.Insert(aero);
+                return "Record successfully created";
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
