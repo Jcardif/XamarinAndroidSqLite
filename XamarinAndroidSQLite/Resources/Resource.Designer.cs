d778d249269c08a0de14493de5644e6a3d85f3da
Jcardif <josh.cardif14@outlook.com>
2018-03-03 17:54:36 +0300

    Layout for viewing all planes

--- XamarinAndroidSQLite/Resources/Resource.Designer.cs
+++ XamarinAndroidSQLite/Resources/Resource.Designer.cs
@@ -2330,8 +2330,8 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f070036
 			public const int SYM = 2131165238;
 			
-			// aapt resource value: 0x7f0700a5
-			public const int action0 = 2131165349;
+			// aapt resource value: 0x7f0700a6
+			public const int action0 = 2131165350;
 			
 			// aapt resource value: 0x7f07007b
 			public const int action_bar = 2131165307;
@@ -2354,17 +2354,17 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f070059
 			public const int action_bar_title = 2131165273;
 			
-			// aapt resource value: 0x7f0700a2
-			public const int action_container = 2131165346;
+			// aapt resource value: 0x7f0700a3
+			public const int action_container = 2131165347;
 			
 			// aapt resource value: 0x7f07007c
 			public const int action_context_bar = 2131165308;
 			
-			// aapt resource value: 0x7f0700a9
-			public const int action_divider = 2131165353;
+			// aapt resource value: 0x7f0700aa
+			public const int action_divider = 2131165354;
 			
-			// aapt resource value: 0x7f0700a3
-			public const int action_image = 2131165347;
+			// aapt resource value: 0x7f0700a4
+			public const int action_image = 2131165348;
 			
 			// aapt resource value: 0x7f070003
 			public const int action_menu_divider = 2131165187;
@@ -2381,11 +2381,11 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f07005b
 			public const int action_mode_close_button = 2131165275;
 			
-			// aapt resource value: 0x7f0700a4
-			public const int action_text = 2131165348;
+			// aapt resource value: 0x7f0700a5
+			public const int action_text = 2131165349;
 			
-			// aapt resource value: 0x7f0700b2
-			public const int actions = 2131165362;
+			// aapt resource value: 0x7f0700b3
+			public const int actions = 2131165363;
 			
 			// aapt resource value: 0x7f07005c
 			public const int activity_chooser_view_content = 2131165276;
@@ -2395,6 +2395,9 @@ namespace XamarinAndroidSQLite
 			
 			// aapt resource value: 0x7f07008c
 			public const int addAeroplane = 2131165324;
+			
+			// aapt resource value: 0x7f07008d
+			public const int aerplanesList = 2131165325;
 			
 			// aapt resource value: 0x7f07006f
 			public const int alertTitle = 2131165295;
@@ -2423,8 +2426,8 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f070062
 			public const int buttonPanel = 2131165282;
 			
-			// aapt resource value: 0x7f0700a6
-			public const int cancel_action = 2131165350;
+			// aapt resource value: 0x7f0700a7
+			public const int cancel_action = 2131165351;
 			
 			// aapt resource value: 0x7f070044
 			public const int center = 2131165252;
@@ -2438,8 +2441,8 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f070072
 			public const int checkbox = 2131165298;
 			
-			// aapt resource value: 0x7f0700ae
-			public const int chronometer = 2131165358;
+			// aapt resource value: 0x7f0700af
+			public const int chronometer = 2131165359;
 			
 			// aapt resource value: 0x7f07004d
 			public const int clip_horizontal = 2131165261;
@@ -2450,20 +2453,20 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f070038
 			public const int collapseActionView = 2131165240;
 			
-			// aapt resource value: 0x7f07008f
-			public const int container = 2131165327;
+			// aapt resource value: 0x7f070090
+			public const int container = 2131165328;
 			
 			// aapt resource value: 0x7f070065
 			public const int contentPanel = 2131165285;
 			
-			// aapt resource value: 0x7f070090
-			public const int coordinator = 2131165328;
-			
-			// aapt resource value: 0x7f07009b
-			public const int createdbBtn = 2131165339;
+			// aapt resource value: 0x7f070091
+			public const int coordinator = 2131165329;
 			
 			// aapt resource value: 0x7f07009c
-			public const int createtableBtn = 2131165340;
+			public const int createdbBtn = 2131165340;
+			
+			// aapt resource value: 0x7f07009d
+			public const int createtableBtn = 2131165341;
 			
 			// aapt resource value: 0x7f07006c
 			public const int custom = 2131165292;
@@ -2477,23 +2480,23 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f07005f
 			public const int default_activity_button = 2131165279;
 			
-			// aapt resource value: 0x7f0700a1
-			public const int deleteDataBtn = 2131165345;
+			// aapt resource value: 0x7f0700a2
+			public const int deleteDataBtn = 2131165346;
 			
-			// aapt resource value: 0x7f070092
-			public const int design_bottom_sheet = 2131165330;
+			// aapt resource value: 0x7f070093
+			public const int design_bottom_sheet = 2131165331;
+			
+			// aapt resource value: 0x7f07009a
+			public const int design_menu_item_action_area = 2131165338;
 			
 			// aapt resource value: 0x7f070099
-			public const int design_menu_item_action_area = 2131165337;
+			public const int design_menu_item_action_area_stub = 2131165337;
 			
 			// aapt resource value: 0x7f070098
-			public const int design_menu_item_action_area_stub = 2131165336;
+			public const int design_menu_item_text = 2131165336;
 			
 			// aapt resource value: 0x7f070097
-			public const int design_menu_item_text = 2131165335;
-			
-			// aapt resource value: 0x7f070096
-			public const int design_navigation_view = 2131165334;
+			public const int design_navigation_view = 2131165335;
 			
 			// aapt resource value: 0x7f07001f
 			public const int disableHome = 2131165215;
@@ -2504,8 +2507,8 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f07002f
 			public const int end = 2131165231;
 			
-			// aapt resource value: 0x7f0700b4
-			public const int end_padder = 2131165364;
+			// aapt resource value: 0x7f0700b5
+			public const int end_padder = 2131165365;
 			
 			// aapt resource value: 0x7f07003e
 			public const int enterAlways = 2131165246;
@@ -2537,11 +2540,11 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f070057
 			public const int forever = 2131165271;
 			
-			// aapt resource value: 0x7f07009f
-			public const int getDataByid = 2131165343;
+			// aapt resource value: 0x7f0700a0
+			public const int getDataByid = 2131165344;
 			
-			// aapt resource value: 0x7f07009e
-			public const int getdatabtn = 2131165342;
+			// aapt resource value: 0x7f07009f
+			public const int getdatabtn = 2131165343;
 			
 			// aapt resource value: 0x7f07000a
 			public const int ghost_view = 2131165194;
@@ -2555,8 +2558,8 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f070061
 			public const int icon = 2131165281;
 			
-			// aapt resource value: 0x7f0700b3
-			public const int icon_group = 2131165363;
+			// aapt resource value: 0x7f0700b4
+			public const int icon_group = 2131165364;
 			
 			// aapt resource value: 0x7f070039
 			public const int ifRoom = 2131165241;
@@ -2564,11 +2567,11 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f07005e
 			public const int image = 2131165278;
 			
-			// aapt resource value: 0x7f0700af
-			public const int info = 2131165359;
+			// aapt resource value: 0x7f0700b0
+			public const int info = 2131165360;
 			
-			// aapt resource value: 0x7f07009d
-			public const int insertRecordBtn = 2131165341;
+			// aapt resource value: 0x7f07009e
+			public const int insertRecordBtn = 2131165342;
 			
 			// aapt resource value: 0x7f070058
 			public const int italic = 2131165272;
@@ -2576,8 +2579,8 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f070000
 			public const int item_touch_helper_previous_elevation = 2131165184;
 			
-			// aapt resource value: 0x7f07008e
-			public const int largeLabel = 2131165326;
+			// aapt resource value: 0x7f07008f
+			public const int largeLabel = 2131165327;
 			
 			// aapt resource value: 0x7f070048
 			public const int left = 2131165256;
@@ -2594,14 +2597,14 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f070060
 			public const int list_item = 2131165280;
 			
-			// aapt resource value: 0x7f0700b7
-			public const int masked = 2131165367;
+			// aapt resource value: 0x7f0700b8
+			public const int masked = 2131165368;
 			
-			// aapt resource value: 0x7f0700a8
-			public const int media_actions = 2131165352;
+			// aapt resource value: 0x7f0700a9
+			public const int media_actions = 2131165353;
 			
-			// aapt resource value: 0x7f0700b5
-			public const int message = 2131165365;
+			// aapt resource value: 0x7f0700b6
+			public const int message = 2131165366;
 			
 			// aapt resource value: 0x7f070030
 			public const int middle = 2131165232;
@@ -2615,8 +2618,8 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f07008b
 			public const int nameEdtTxt = 2131165323;
 			
-			// aapt resource value: 0x7f070095
-			public const int navigation_header_container = 2131165333;
+			// aapt resource value: 0x7f070096
+			public const int navigation_header_container = 2131165334;
 			
 			// aapt resource value: 0x7f07003a
 			public const int never = 2131165242;
@@ -2627,14 +2630,14 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f07001d
 			public const int normal = 2131165213;
 			
-			// aapt resource value: 0x7f0700b1
-			public const int notification_background = 2131165361;
+			// aapt resource value: 0x7f0700b2
+			public const int notification_background = 2131165362;
+			
+			// aapt resource value: 0x7f0700ac
+			public const int notification_main_column = 2131165356;
 			
 			// aapt resource value: 0x7f0700ab
-			public const int notification_main_column = 2131165355;
-			
-			// aapt resource value: 0x7f0700aa
-			public const int notification_main_column_container = 2131165354;
+			public const int notification_main_column_container = 2131165355;
 			
 			// aapt resource value: 0x7f07004b
 			public const int parallax = 2131165259;
@@ -2660,11 +2663,11 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f070049
 			public const int right = 2131165257;
 			
-			// aapt resource value: 0x7f0700b0
-			public const int right_icon = 2131165360;
+			// aapt resource value: 0x7f0700b1
+			public const int right_icon = 2131165361;
 			
-			// aapt resource value: 0x7f0700ac
-			public const int right_side = 2131165356;
+			// aapt resource value: 0x7f0700ad
+			public const int right_side = 2131165357;
 			
 			// aapt resource value: 0x7f07000c
 			public const int save_image_matrix = 2131165196;
@@ -2738,14 +2741,14 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f070024
 			public const int showTitle = 2131165220;
 			
-			// aapt resource value: 0x7f07008d
-			public const int smallLabel = 2131165325;
+			// aapt resource value: 0x7f07008e
+			public const int smallLabel = 2131165326;
+			
+			// aapt resource value: 0x7f070095
+			public const int snackbar_action = 2131165333;
 			
 			// aapt resource value: 0x7f070094
-			public const int snackbar_action = 2131165332;
-			
-			// aapt resource value: 0x7f070093
-			public const int snackbar_text = 2131165331;
+			public const int snackbar_text = 2131165332;
 			
 			// aapt resource value: 0x7f070042
 			public const int snap = 2131165250;
@@ -2768,8 +2771,8 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f07004a
 			public const int start = 2131165258;
 			
-			// aapt resource value: 0x7f0700a7
-			public const int status_bar_latest_event_content = 2131165351;
+			// aapt resource value: 0x7f0700a8
+			public const int status_bar_latest_event_content = 2131165352;
 			
 			// aapt resource value: 0x7f070075
 			public const int submenuarrow = 2131165301;
@@ -2795,8 +2798,8 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f07008a
 			public const int textVie = 2131165322;
 			
-			// aapt resource value: 0x7f07009a
-			public const int text_input_password_toggle = 2131165338;
+			// aapt resource value: 0x7f07009b
+			public const int text_input_password_toggle = 2131165339;
 			
 			// aapt resource value: 0x7f070014
 			public const int textinput_counter = 2131165204;
@@ -2804,8 +2807,8 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f070015
 			public const int textinput_error = 2131165205;
 			
-			// aapt resource value: 0x7f0700ad
-			public const int time = 2131165357;
+			// aapt resource value: 0x7f0700ae
+			public const int time = 2131165358;
 			
 			// aapt resource value: 0x7f07001b
 			public const int title = 2131165211;
@@ -2822,8 +2825,8 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f07006d
 			public const int topPanel = 2131165293;
 			
-			// aapt resource value: 0x7f070091
-			public const int touch_outside = 2131165329;
+			// aapt resource value: 0x7f070092
+			public const int touch_outside = 2131165330;
 			
 			// aapt resource value: 0x7f07000f
 			public const int transition_current_scene = 2131165199;
@@ -2846,8 +2849,8 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f070009
 			public const int up = 2131165193;
 			
-			// aapt resource value: 0x7f0700a0
-			public const int updatedataBtn = 2131165344;
+			// aapt resource value: 0x7f0700a1
+			public const int updatedataBtn = 2131165345;
 			
 			// aapt resource value: 0x7f070025
 			public const int useLogo = 2131165221;
@@ -2855,8 +2858,8 @@ namespace XamarinAndroidSQLite
 			// aapt resource value: 0x7f070016
 			public const int view_offset_helper = 2131165206;
 			
-			// aapt resource value: 0x7f0700b6
-			public const int visible = 2131165366;
+			// aapt resource value: 0x7f0700b7
+			public const int visible = 2131165367;
 			
 			// aapt resource value: 0x7f07003b
 			public const int withText = 2131165243;
@@ -3005,109 +3008,112 @@ namespace XamarinAndroidSQLite
 			public const int AddRecord = 2130903067;
 			
 			// aapt resource value: 0x7f03001c
-			public const int design_bottom_navigation_item = 2130903068;
+			public const int AllAeroplanes = 2130903068;
 			
 			// aapt resource value: 0x7f03001d
-			public const int design_bottom_sheet_dialog = 2130903069;
+			public const int design_bottom_navigation_item = 2130903069;
 			
 			// aapt resource value: 0x7f03001e
-			public const int design_layout_snackbar = 2130903070;
+			public const int design_bottom_sheet_dialog = 2130903070;
 			
 			// aapt resource value: 0x7f03001f
-			public const int design_layout_snackbar_include = 2130903071;
+			public const int design_layout_snackbar = 2130903071;
 			
 			// aapt resource value: 0x7f030020
-			public const int design_layout_tab_icon = 2130903072;
+			public const int design_layout_snackbar_include = 2130903072;
 			
 			// aapt resource value: 0x7f030021
-			public const int design_layout_tab_text = 2130903073;
+			public const int design_layout_tab_icon = 2130903073;
 			
 			// aapt resource value: 0x7f030022
-			public const int design_menu_item_action_area = 2130903074;
+			public const int design_layout_tab_text = 2130903074;
 			
 			// aapt resource value: 0x7f030023
-			public const int design_navigation_item = 2130903075;
+			public const int design_menu_item_action_area = 2130903075;
 			
 			// aapt resource value: 0x7f030024
-			public const int design_navigation_item_header = 2130903076;
+			public const int design_navigation_item = 2130903076;
 			
 			// aapt resource value: 0x7f030025
-			public const int design_navigation_item_separator = 2130903077;
+			public const int design_navigation_item_header = 2130903077;
 			
 			// aapt resource value: 0x7f030026
-			public const int design_navigation_item_subheader = 2130903078;
+			public const int design_navigation_item_separator = 2130903078;
 			
 			// aapt resource value: 0x7f030027
-			public const int design_navigation_menu = 2130903079;
+			public const int design_navigation_item_subheader = 2130903079;
 			
 			// aapt resource value: 0x7f030028
-			public const int design_navigation_menu_item = 2130903080;
+			public const int design_navigation_menu = 2130903080;
 			
 			// aapt resource value: 0x7f030029
-			public const int design_text_input_password_icon = 2130903081;
+			public const int design_navigation_menu_item = 2130903081;
 			
 			// aapt resource value: 0x7f03002a
-			public const int Home = 2130903082;
+			public const int design_text_input_password_icon = 2130903082;
 			
 			// aapt resource value: 0x7f03002b
-			public const int notification_action = 2130903083;
+			public const int Home = 2130903083;
 			
 			// aapt resource value: 0x7f03002c
-			public const int notification_action_tombstone = 2130903084;
+			public const int notification_action = 2130903084;
 			
 			// aapt resource value: 0x7f03002d
-			public const int notification_media_action = 2130903085;
+			public const int notification_action_tombstone = 2130903085;
 			
 			// aapt resource value: 0x7f03002e
-			public const int notification_media_cancel_action = 2130903086;
+			public const int notification_media_action = 2130903086;
 			
 			// aapt resource value: 0x7f03002f
-			public const int notification_template_big_media = 2130903087;
+			public const int notification_media_cancel_action = 2130903087;
 			
 			// aapt resource value: 0x7f030030
-			public const int notification_template_big_media_custom = 2130903088;
+			public const int notification_template_big_media = 2130903088;
 			
 			// aapt resource value: 0x7f030031
-			public const int notification_template_big_media_narrow = 2130903089;
+			public const int notification_template_big_media_custom = 2130903089;
 			
 			// aapt resource value: 0x7f030032
-			public const int notification_template_big_media_narrow_custom = 2130903090;
+			public const int notification_template_big_media_narrow = 2130903090;
 			
 			// aapt resource value: 0x7f030033
-			public const int notification_template_custom_big = 2130903091;
+			public const int notification_template_big_media_narrow_custom = 2130903091;
 			
 			// aapt resource value: 0x7f030034
-			public const int notification_template_icon_group = 2130903092;
+			public const int notification_template_custom_big = 2130903092;
 			
 			// aapt resource value: 0x7f030035
-			public const int notification_template_lines_media = 2130903093;
+			public const int notification_template_icon_group = 2130903093;
 			
 			// aapt resource value: 0x7f030036
-			public const int notification_template_media = 2130903094;
+			public const int notification_template_lines_media = 2130903094;
 			
 			// aapt resource value: 0x7f030037
-			public const int notification_template_media_custom = 2130903095;
+			public const int notification_template_media = 2130903095;
 			
 			// aapt resource value: 0x7f030038
-			public const int notification_template_part_chronometer = 2130903096;
+			public const int notification_template_media_custom = 2130903096;
 			
 			// aapt resource value: 0x7f030039
-			public const int notification_template_part_time = 2130903097;
+			public const int notification_template_part_chronometer = 2130903097;
 			
 			// aapt resource value: 0x7f03003a
-			public const int select_dialog_item_material = 2130903098;
+			public const int notification_template_part_time = 2130903098;
 			
 			// aapt resource value: 0x7f03003b
-			public const int select_dialog_multichoice_material = 2130903099;
+			public const int select_dialog_item_material = 2130903099;
 			
 			// aapt resource value: 0x7f03003c
-			public const int select_dialog_singlechoice_material = 2130903100;
+			public const int select_dialog_multichoice_material = 2130903100;
 			
 			// aapt resource value: 0x7f03003d
-			public const int support_simple_spinner_dropdown_item = 2130903101;
+			public const int select_dialog_singlechoice_material = 2130903101;
 			
 			// aapt resource value: 0x7f03003e
-			public const int tooltip = 2130903102;
+			public const int support_simple_spinner_dropdown_item = 2130903102;
+			
+			// aapt resource value: 0x7f03003f
+			public const int tooltip = 2130903103;
 			
 			static Layout()
 			{
